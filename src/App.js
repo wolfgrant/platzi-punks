3fbd253444b45e4b191b31801f941292d4c8fa9c
wolfgrant <47611838+wolfgrant@users.noreply.github.com>
2022-09-01 21:38:57 -0500

    feat: Se crea implementación de ethereum web3 con el proveedor, en este caso metamask

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './views/home';
+import { useEffect } from 'react';
+import Web3 from "web3/dist/web3.min";
 
 function App() {
+
+  useEffect(() => {
+    if (window.ethereum) {
+      /* window.ethereum.request({ method: 'eth_requestAccounts' }).then(accounts => console.log(accounts)) */
+
+      const web3 = new Web3(window.ethereum);
+      web3.eth.requestAccounts().then(console.log);
+    }
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Routes>
+      <Route path="/" exact element={<Home />} />
+    </Routes>
   );
 }
 
